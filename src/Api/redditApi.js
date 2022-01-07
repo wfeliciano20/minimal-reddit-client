dcac77cbcc4d04d573f8cc93b11294317d8a5722
wfeliciano20 <william.feliciano2@gmail.com>
2022-01-07 14:09:27 -0500

    fixed json return

--- src/Api/redditApi.js
+++ src/Api/redditApi.js
@@ -2,19 +2,19 @@ const API_ROOT = 'https://www.reddit.com';
 
 export const getSubreddits = async() => {
     const response = await fetch(`${API_ROOT}/subreddits.json`)
-    const json = response.json();
+    const json = await response.json();
     return json.data.children.map(subreddit => subreddit.data);
 
 }
 
 export const getPostForSubreddit = async(subreddit) => {
     const response = fetch(`${API_ROOT}${subreddit}.json`);
-    const json = response.json();
+    const json = await response.json();
     return json.data.children.map(post => post.data);
 }
 
 export const getCommentsForPost = async(permalink) => {
     const response = await fetch(`${API_ROOT}${permalink}.json`)
-    const json = response.json();
+    const json = await response.json();
     return json[1].data.children.map((subreddit) => subreddit.data);
 }
