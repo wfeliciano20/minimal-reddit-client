64bad89e6fb941e31e50848916a3cef2ecfb5338
wfeliciano20 <william.feliciano2@gmail.com>
2022-01-10 16:19:02 -0500

    tweak navbar ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,14 @@ const Navbar = () => {
   const [termLocal,setTermLocal] = useState('');
 
   return (
-    <div className={`h-[74px] rounded-2xl grid-area-navbar lg:col-span-3 shadow-xl drop-shadow-2xl `}>
+    <div className={`h-[74px] rounded-xl grid-area-navbar lg:col-span-3 border-b-2 shadow-lg`}>
         <div className="flex flex-row justify-around mt-5 md:mt-3 px-5">
           <div className="flex flex-row items-center justify-center">
             <FaReddit className="text-3xl md:text-4xl lg:text-5xl pr-2 text-blue-600"/>
             <p className="text-sm md:text-xl lg:text-2xl">Minimal Reddit</p>
           </div>
           <div className="search flex flex-row">
-            <HiOutlineSearch className="text-4xl pr-2 text-blue-600 mt-2 md:mt-3"/>
+            <HiOutlineSearch className="text-3xl md:text-4xl pr-2 text-blue-600 mt-2 md:mt-3"/>
             <input value={termLocal} onChange={e => setTermLocal(e.target.value)} type="text" placeholder="Search" className=" w-[5rem] md:w-[15rem] rounded-xl border-2 h-10 mt-2 md:mt-3 p-2 border-gray-300 border-solid" />
           </div>
           <div className="w-52 h-[64px]"></div>
