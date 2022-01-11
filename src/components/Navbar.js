06a08d860958a5d301400436a86b5590a81bcedd
wfeliciano20 <william.feliciano2@gmail.com>
2022-01-10 19:37:38 -0500

    tweak navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,11 +27,11 @@ const Navbar = () => {
             <p className="text-sm md:text-xl lg:text-2xl">Minimal Reddit</p>
           </div>
           <div className="search flex flex-row">
-            <form onSubmit={onSearchSubmit}>
-              <button type='submit' onSubmit={onSearchSubmit} aria-label="Search">
-                  <HiOutlineSearch className="text-3xl md:text-4xl pr-2 text-blue-600 mt-2 md:mt-3"/>
+            <form className="flex flex-row" onSubmit={onSearchSubmit}>
+              <button type='submit' onSubmit={onSearchSubmit} aria-label="Search" className="mb-1">
+                  <HiOutlineSearch className="text-3xl md:text-4xl pr-2 text-blue-600 "/>
               </button>
-              <input value={termLocal} onChange={e => setTermLocal(e.target.value)} type="text" placeholder="Search" className=" w-[5rem] md:w-[15rem] rounded-xl border-2 h-10 mt-2 md:mt-3 p-2 border-gray-300 border-solid" />
+              <input value={termLocal} onChange={e => setTermLocal(e.target.value)} type="text" placeholder="Search" className=" w-[5rem] md:w-[15rem] rounded-xl border-2 h-10 mt-2 md:mt-2 p-2 border-gray-300 border-solid" />
             </form>
           </div>
           <div className="w-52 h-[64px]"></div>
